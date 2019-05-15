81db2853884e5e8d0b9d4b8b374a25b7dd39c92a
SleepingFox <seanloganking@gmail.com>
2019-05-15 02:29:52 -0400

    added tabs

--- SamplePrograms/Fib.asm
+++ SamplePrograms/Fib.asm
@@ -1,10 +1,10 @@
-MOV A, 1
-MOV B, 0
-PRINT B
+  MOV A, 1
+  MOV B, 0
+  PRINT B
 
 .loop2:
-PRINT A
-MOV C, A
-ADD A, B
-MOV B, C
-JMP .loop2
+  PRINT A
+  MOV C, A
+  ADD A, B
+  MOV B, C
+  JMP .loop2
