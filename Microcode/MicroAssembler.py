eab873b41c1582a73bc5754d6ea83bd3a1c8ae0c
SleepingFox <seanloganking@gmail.com>
2019-06-01 01:20:55 -0400

    Create MicroAssembler.py

---
+++ Microcode/MicroAssembler.py
@@ -0,0 +1,29 @@
+# Imports
+# ===========================================================================
+
+
+import os
+from os import listdir
+from os.path import isfile, join
+import time
+
+# Function declarations
+# ===========================================================================
+
+
+# Remove all files in given directory
+def removeAllFilesInDirectory(directory):
+    onlyfiles = [f for f in listdir(directory) if isfile(join(directory, f))]
+    for i in range(0, len(onlyfiles)):
+        os.remove(f"{directory}{onlyfiles[i]}")
+
+
+# Start of main program
+# ===========================================================================
+
+removeAllFilesInDirectory("./Output")
+
+
+with open(f"./Microcode.txt") as input:
+    content = input.readlines()
+    print(content)
