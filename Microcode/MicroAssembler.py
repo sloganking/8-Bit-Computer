31021e7fd5419b2207799f9d2a01fb2e4eefa73d
SleepingFox <seanloganking@gmail.com>
2019-06-01 01:42:32 -0400

    Start of Microassembling

--- Microcode/MicroAssembler.py
+++ Microcode/MicroAssembler.py
@@ -21,9 +21,29 @@ def removeAllFilesInDirectory(directory):
 # Start of main program
 # ===========================================================================
 
-removeAllFilesInDirectory("./Output")
+removeAllFilesInDirectory("./Output/")
 
 
 with open(f"./Microcode.txt") as input:
-    content = input.readlines()
-    print(content)
+    with open(f"./Output/instrucToBinary.json", "w") as output:
+        content = input.readlines()\
+
+
+        # Start JSON object
+        print(f"{{", file=output)
+        # print(content)
+        microAddress = 0
+        for x in range(0, len(content)):
+            tokens = str.split(content[x])
+            strToPrint = ""
+
+            if len(tokens) > 0:
+                # if first char of string is ";", ignore line
+                if tokens[0][0] == ";":
+                    print("test")
+                # if last char of first token is ":", instruction detected
+                elif tokens[0][len(tokens[0]) - 1] == ":":
+                    print("is :")
+
+        # End JSON object
+        print(f"}}", file=output)
