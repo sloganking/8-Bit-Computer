99ac8f8bc340e7ebd2f2d21faf0194de5a42e548
SleepingFox88 <seanloganking@gmail.com>
2019-06-13 01:45:06 -0400

    expanded "test.asm" demo program
    also assembled it

--- Assembler/test.asm
+++ Assembler/test.asm
@@ -1,3 +1,21 @@
+counting:
+      MOV A, 0
+
+.countLoop:
+    PRINT A
+    INC A
+    CMP A, 11
+    JNC .countLoop
+
+doubling:
+  MOV A, 1
+  
+.doublingLoop:
+  PRINT A
+  SHL A
+  JNC .doublingLoop
+  
+fib:
   MOV A, 1
   MOV B, 0
   PRINT B
