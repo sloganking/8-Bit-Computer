510a989d4b7609770a6758238306101bd94646f7
SleepingFox88 <seanloganking@gmail.com>
2019-06-29 01:34:10 -0400

    Create largeDemo.asm

---
+++ SamplePrograms/largeDemo.asm
@@ -0,0 +1,29 @@
+counting:
+  MOV A, 0
+
+.countLoop:
+  PRINT A
+  INC A
+  CMP A, 11
+  JNC .countLoop
+
+doubling:
+  MOV A, 1
+  
+.doublingLoop:
+  PRINT A
+  SHL A
+  JNC .doublingLoop
+  
+fib:
+  MOV A, 1
+  MOV B, 0
+  PRINT B
+
+.fibLoop:
+  PRINT A
+  MOV C, A
+  ADD A, B
+  MOV B, C
+  JNC .fibLoop
+  HLT
