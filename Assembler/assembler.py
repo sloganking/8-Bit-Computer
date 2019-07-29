a49d1629c7a80b2fc8ef442319705abafaee7d74
SleepingFox <seanloganking@gmail.com>
2019-07-29 16:32:12 -0400

    create assembler class

--- Assembler/assembler.py
+++ Assembler/assembler.py
@@ -11,160 +11,153 @@ import time
 # ===========================================================================
 
 
-def removeAllFilesInDirectory(directory):
-    onlyfiles = [f for f in listdir(directory) if isfile(join(directory, f))]
-    for i in range(0, len(onlyfiles)):
-        os.remove(f"{directory}{onlyfiles[i]}")
+class assembler:
 
+    def removeAllFilesInDirectory(directory):
+        onlyfiles = [f for f in listdir(
+            directory) if isfile(join(directory, f))]
+        for i in range(0, len(onlyfiles)):
+            os.remove(f"{directory}{onlyfiles[i]}")
 
-def RepresentsInt(s):
-    try:
-        int(s)
-        return True
-    except ValueError:
-        return False
-
-
-def returnType(token):
-    regCharacters = ["A", "B", "C", "D"]
-    isAddress = False
-    if token.startswith('[') and token.endswith(']'):
-        isAddress = True
-        token = token.replace("[", "")
-        token = token.replace("]", "")
-
-    if "," in token:
-        token = token.replace(",", "")
-    # print("token:   " + token)
-    if RepresentsInt(token):
-        if isAddress:
-            return "[const]"
-        else:
-            return "const"
-    elif len(token) == 1 and token in regCharacters:
-        if isAddress:
-            return "[reg]"
-        else:
-            return "reg"
-    elif isALabel(token):
-        if isAddress:
-            return "[const]"
-        else:
-            return "const"
-
-
-def tokensToInstruc(tokens):
-    instruc = ""
-    if len(tokens) > 0:
-        instruc = instruc + tokens[0]
-    if len(tokens) > 1:
-        instruc = instruc + "_" + returnType(tokens[1])
-    if len(tokens) > 2:
-        instruc = instruc + "_" + returnType(tokens[2])
-    return instruc
-
-
-def getListOfLabels():
-    with open(f"./test.asm") as labelInput:
-        listOfLabels = []
-        labelContent = labelInput.readlines()
-        for lx in range(0, len(labelContent)):
-            labelTokens = str.split(labelContent[lx])
-            if len(labelTokens) > 0:
-                if str(labelTokens[0][-1:]) == ":":
-                    listOfLabels.append(labelTokens[0].replace(":", ""))
-        return listOfLabels
-
-
-def getLabelNumbers():
-    labelNumbers = []
-    with open(f"./test.asm") as labelInput:
-        currentByte = 0
-        labelContent = labelInput.readlines()
-        for lx in range(0, len(labelContent)):
-
-            labelTokens = str.split(labelContent[lx])
-            if len(labelTokens) > 0:
-                if str(labelTokens[0][-1:]) == ":":
-                    labelNumbers.append(currentByte)
-                elif instrucToBinary(tokensToInstruc(labelTokens)):
-                    currentByte += len(labelTokens)
-    return labelNumbers
-
-
-def isALabel(string):
-    if string in listOfLabels:
-        return True
-    else:
-        return False
-
-
-def instrucToBinary(string):
-    with open(f"./instrucToBinary.json") as json_data:
-        instrucToBinary = json.load(json_data)
+    def RepresentsInt(s):
         try:
-            return instrucToBinary[string]
-        except:
+            int(s)
+            return True
+        except ValueError:
             return False
 
+    def returnType(token):
+        regCharacters = ["A", "B", "C", "D"]
+        isAddress = False
+        if token.startswith('[') and token.endswith(']'):
+            isAddress = True
+            token = token.replace("[", "")
+            token = token.replace("]", "")
 
-def regToBinary(reg):
-    reg = reg.replace(",", "")
-    if reg == "A":
-        return 0
-    elif reg == "B":
-        return 1
-    elif reg == "C":
-        return 2
-    elif reg == "D":
-        return 3
-    else:
-        return False
+        if "," in token:
+            token = token.replace(",", "")
+        # print("token:   " + token)
+        if RepresentsInt(token):
+            if isAddress:
+                return "[const]"
+            else:
+                return "const"
+        elif len(token) == 1 and token in regCharacters:
+            if isAddress:
+                return "[reg]"
+            else:
+                return "reg"
+        elif isALabel(token):
+            if isAddress:
+                return "[const]"
+            else:
+                return "const"
 
+    def tokensToInstruc(tokens):
+        instruc = ""
+        if len(tokens) > 0:
+            instruc = instruc + tokens[0]
+        if len(tokens) > 1:
+            instruc = instruc + "_" + returnType(tokens[1])
+        if len(tokens) > 2:
+            instruc = instruc + "_" + returnType(tokens[2])
+        return instruc
 
-def constToBinary(const):
-    if isALabel(const):
-        return labelNumbers[listOfLabels.index(const)]
-    else:
-        return int(const.replace(",", ""))
+    def getListOfLabels():
+        with open(f"./test.asm") as labelInput:
+            listOfLabels = []
+            labelContent = labelInput.readlines()
+            for lx in range(0, len(labelContent)):
+                labelTokens = str.split(labelContent[lx])
+                if len(labelTokens) > 0:
+                    if str(labelTokens[0][-1:]) == ":":
+                        listOfLabels.append(labelTokens[0].replace(":", ""))
+            return listOfLabels
 
-# Initialization
-# ===========================================================================
+    def getLabelNumbers():
+        labelNumbers = []
+        with open(f"./test.asm") as labelInput:
+            currentByte = 0
+            labelContent = labelInput.readlines()
+            for lx in range(0, len(labelContent)):
 
+                labelTokens = str.split(labelContent[lx])
+                if len(labelTokens) > 0:
+                    if str(labelTokens[0][-1:]) == ":":
+                        labelNumbers.append(currentByte)
+                    elif instrucToBinary(tokensToInstruc(labelTokens)):
+                        currentByte += len(labelTokens)
+        return labelNumbers
 
-removeAllFilesInDirectory("./Output/")
+    def isALabel(string):
+        if string in listOfLabels:
+            return True
+        else:
+            return False
 
-with open(f"./test.asm") as input:
-    with open(f"./Output/machineCode.bin", "wb") as output:
-        content = input.readlines()
-        listOfLabels = getListOfLabels()
-        labelNumbers = getLabelNumbers()
-        machineCodeBytes = bytearray()
+    def instrucToBinary(string):
+        with open(f"./instrucToBinary.json") as json_data:
+            instrucToBinary = json.load(json_data)
+            try:
+                return instrucToBinary[string]
+            except:
+                return False
 
-# Start of main program
-# ===========================================================================
+    def regToBinary(reg):
+        reg = reg.replace(",", "")
+        if reg == "A":
+            return 0
+        elif reg == "B":
+            return 1
+        elif reg == "C":
+            return 2
+        elif reg == "D":
+            return 3
+        else:
+            return False
 
-        for x in range(0, len(content)):
-            tokens = str.split(content[x])
-            instruc = tokensToInstruc(tokens)
-            instructionBytes = []
+    def constToBinary(const):
+        if isALabel(const):
+            return labelNumbers[listOfLabels.index(const)]
+        else:
+            return int(const.replace(",", ""))
 
-            # if "tokens" represnt a valid instruction
-            if instrucToBinary(tokensToInstruc(tokens)):
-                machineCodeBytes.append(
-                    int(instrucToBinary(tokensToInstruc(tokens))))
-                if len(tokens) > 1:
-                    if "reg" in returnType(tokens[1]):
-                        machineCodeBytes.append(regToBinary(tokens[1]))
-                    elif "const" in returnType(tokens[1]):
-                        machineCodeBytes.append(constToBinary(tokens[1]))
-                if len(tokens) > 2:
-                    if "reg" in returnType(tokens[2]):
-                        machineCodeBytes.append(regToBinary(tokens[2]))
-                    elif "const" in returnType(tokens[2]):
-                        machineCodeBytes.append(constToBinary(tokens[2]))
+    # Initialization
+    # ===========================================================================
 
-        output.write(machineCodeBytes)
-        print("Assembler finished")
-        print("wrote " + str(len(machineCodeBytes)) + " bytes to output")
-        print(machineCodeBytes)
+    removeAllFilesInDirectory("./Output/")
+
+    with open(f"./test.asm") as input:
+        with open(f"./Output/machineCode.bin", "wb") as output:
+            content = input.readlines()
+            listOfLabels = getListOfLabels()
+            labelNumbers = getLabelNumbers()
+            machineCodeBytes = bytearray()
+
+    # Start of main program
+    # ===========================================================================
+
+            for x in range(0, len(content)):
+                tokens = str.split(content[x])
+                instruc = tokensToInstruc(tokens)
+                instructionBytes = []
+
+                # if "tokens" represnt a valid instruction
+                if instrucToBinary(tokensToInstruc(tokens)):
+                    machineCodeBytes.append(
+                        int(instrucToBinary(tokensToInstruc(tokens))))
+                    if len(tokens) > 1:
+                        if "reg" in returnType(tokens[1]):
+                            machineCodeBytes.append(regToBinary(tokens[1]))
+                        elif "const" in returnType(tokens[1]):
+                            machineCodeBytes.append(constToBinary(tokens[1]))
+                    if len(tokens) > 2:
+                        if "reg" in returnType(tokens[2]):
+                            machineCodeBytes.append(regToBinary(tokens[2]))
+                        elif "const" in returnType(tokens[2]):
+                            machineCodeBytes.append(constToBinary(tokens[2]))
+
+            output.write(machineCodeBytes)
+            print("Assembler finished")
+            print("wrote " + str(len(machineCodeBytes)) + " bytes to output")
+            print(machineCodeBytes)
