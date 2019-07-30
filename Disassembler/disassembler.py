c1991f272d8a15c5620dd18d71cda4e7daf815d8
SleepingFox <seanloganking@gmail.com>
2019-07-29 22:31:15 -0400

    disassembler class

--- Disassembler/disassembler.py
+++ Disassembler/disassembler.py
@@ -11,149 +11,143 @@ import time
 # ===========================================================================
 
 
-def removeAllFilesInDirectory(directory: str):
-    onlyfiles = [f for f in listdir(directory) if isfile(join(directory, f))]
-    for i in range(0, len(onlyfiles)):
-        os.remove(f"{directory}{onlyfiles[i]}")
+class disassembler:
 
+    def __init__(self):
+        pass
 
-# takes machineCode bytes and returns a string of assembly
-def bytesToInstruc(bytes: list):
-    params = binaryToInstrucLayout(bytes[0])
-    tokenedParams = params.split("_")
+    # takes machineCode bytes and returns a string of assembly
+    def bytesToInstruc(self, bytes: list):
+        params = self.binaryToInstrucLayout(bytes[0])
+        tokenedParams = params.split("_")
 
-    instructionString = ""
-    instructionString = instructionString + \
-        nameOfInstuc(binaryToInstrucLayout(bytes[0]))
+        instructionString = ""
+        instructionString = instructionString + \
+            self.nameOfInstuc(self.binaryToInstrucLayout(bytes[0]))
 
-    # x can be 1 and 2
-    for x in range(1, 3):
-        operand = ""
-        if len(tokenedParams) > x:
-            if x == 2:
-                instructionString = instructionString + ","
+        # x can be 1 and 2
+        for x in range(1, 3):
+            operand = ""
+            if len(tokenedParams) > x:
+                if x == 2:
+                    instructionString = instructionString + ","
 
-            if params in firstOperandShouldBeLabel and x == 1:
-                operand = getLabelFor(bytes[x])
-                instructionString = instructionString + " " + operand
-
-            elif "reg" in tokenedParams[x]:
-                if binaryIsReg(bytes[x]):
-                    operand = tokenedParams[x]
-                    operand = operand.replace("reg", binaryToReg(bytes[x]))
+                if params in self.firstOperandShouldBeLabel and x == 1:
+                    operand = self.getLabelFor(bytes[x])
                     instructionString = instructionString + " " + operand
-            elif "const" in tokenedParams[x]:
-                operand = tokenedParams[x]
-                operand = operand.replace("const", str(bytes[x]))
-                instructionString = instructionString + " " + operand
 
-    return instructionString
+                elif "reg" in tokenedParams[x]:
+                    if self.binaryIsReg(bytes[x]):
+                        operand = tokenedParams[x]
+                        operand = operand.replace(
+                            "reg", self.binaryToReg(bytes[x]))
+                        instructionString = instructionString + " " + operand
+                elif "const" in tokenedParams[x]:
+                    operand = tokenedParams[x]
+                    operand = operand.replace("const", str(bytes[x]))
+                    instructionString = instructionString + " " + operand
 
+        return instructionString
 
-def getLabelFor(number: int):
+    def getLabelFor(self, number: int):
 
-    # existing label
-    if number in labelValues:
-        return "l" + str(labels[labelValues.index(number)])
-    # label does not exist
-    else:
-        labels.append(len(labels))
-        labelValues.append(number)
-        return "l" + str(len(labels) - 1)
+        # existing label
+        if number in self.labelValues:
+            return "l" + str(self.labels[self.labelValues.index(number)])
+        # label does not exist
+        else:
+            self.labels.append(len(self.labels))
+            self.labelValues.append(number)
+            return "l" + str(len(self.labels) - 1)
 
+    def binaryToReg(self, binary: int):
+        if binary == 0:
+            return "A"
+        elif binary == 1:
+            return "B"
+        elif binary == 2:
+            return "C"
+        elif binary == 3:
+            return "D"
+        else:
+            assert (False), f"No reg for given binary: {binary}"
 
-def binaryToReg(binary: int):
-    if binary == 0:
-        return "A"
-    elif binary == 1:
-        return "B"
-    elif binary == 2:
-        return "C"
-    elif binary == 3:
-        return "D"
-    else:
-        assert (False), f"No reg for given binary: {binary}"
+    def binaryIsReg(self, binary: int):
+        try:
+            self.binaryToReg(binary)
+            return True
+        except:
+            return False
 
+    # returns string of instruction's name with it's paramater(s) types
 
-def binaryIsReg(binary: int):
-    try:
-        binaryToReg(binary)
-        return True
-    except:
-        return False
+    def binaryToInstrucLayout(self, binary: int):
+        binary = str(binary)
+        return self.instrucNames[self.instrucNumbers.index(binary)]
 
+    def nameOfInstuc(self, instruc: str):
+        tokens = instruc.split("_")
+        return tokens[0]
 
-# returns string of instruction's name with it's paramater(s) types
-def binaryToInstrucLayout(binary: int):
-    binary = str(binary)
-    return instrucNames[instrucNumbers.index(binary)]
+    # Initialization
+    # ===========================================================================
 
+    def disassemble(self, inputDir, outputDir):
 
-def nameOfInstuc(instruc: str):
-    tokens = instruc.split("_")
-    return tokens[0]
+        # initialize known label number
+        self.labels = []
+        self.labelValues = []
 
-# Initialization
-# ===========================================================================
+        self.firstOperandShouldBeLabel = ["JMP_const",
+                                          "JNC_const", "JC_const", "JNZ_const", "JZ_const"]
 
+        # create byteArray with all file bytes
+        with open(inputDir, "rb") as f:
+            byte = f.read()
 
-removeAllFilesInDirectory("./Output/")
+        # create and load binaryToIncruc array
+        with open(f"./instrucToBinary.json") as json_data:
+            instrucDict = json.load(json_data)
+            # print(instrucToBinary[0])
 
-# initialize known label number
+        self.instrucNames = list(instrucDict.keys())
 
-labels = []
-labelValues = []
+        self.instrucNumbers = []
+        for instrucName in self.instrucNames:
+            self.instrucNumbers.append(instrucDict[instrucName])
 
-firstOperandShouldBeLabel = ["JMP_const",
-                             "JNC_const", "JC_const", "JNZ_const", "JZ_const"]
+        # Start of main program
+        # ===========================================================================
 
-# create byteArray with all file bytes
-with open("./machineCode.bin", "rb") as f:
-    byte = f.read()
+        # print(self.bytesToInstruc([3, 0, 1]))
 
+        with open(outputDir, "w") as output:
+            instructionBundles = []
+            i = 0
+            while(i < len(byte)):
+                instrucLayout = self.binaryToInstrucLayout(byte[i])
+                layoutTokens = instrucLayout.split("_")
 
-# create and load binaryToIncruc array
-with open(f"./instrucToBinary.json") as json_data:
-    instrucDict = json.load(json_data)
-    # print(instrucToBinary[0])
+                instructionBundle = []
+                for x in range(len(layoutTokens)):
+                    instructionBundle.append(byte[i + x])
 
-instrucNames = list(instrucDict.keys())
+                # write assembly line to output file
+                # output.write(self.bytesToInstruc(instructionBundle) + "\n")
+                instructionBundles.append(
+                    self.bytesToInstruc(instructionBundle))
 
-instrucNumbers = []
-for instrucName in instrucNames:
-    instrucNumbers.append(instrucDict[instrucName])
+                # sets i to location of next instruction.
+                i = i + len(layoutTokens)
 
-# Start of main program
-# ===========================================================================
+            i = 0
+            for x in range(len(instructionBundles)):
 
-# print(bytesToInstruc([3, 0, 1]))
+                if i in self.labelValues:
+                    output.write(self.getLabelFor(i) + ":" + "\n")
 
-with open(f"./Output/test.asm", "w") as output:
-    instructionBundles = []
-    i = 0
-    while(i < len(byte)):
-        instrucLayout = binaryToInstrucLayout(byte[i])
-        layoutTokens = instrucLayout.split("_")
+                output.write("\t" + instructionBundles[x] + "\n")
 
-        instructionBundle = []
-        for x in range(len(layoutTokens)):
-            instructionBundle.append(byte[i + x])
-
-        # write assembly line to output file
-        # output.write(bytesToInstruc(instructionBundle) + "\n")
-        instructionBundles.append(bytesToInstruc(instructionBundle))
-
-        # sets i to location of next instruction.
-        i = i + len(layoutTokens)
-
-    i = 0
-    for x in range(len(instructionBundles)):
-
-        if i in labelValues:
-            output.write(getLabelFor(i) + ":" + "\n")
-
-        output.write("\t" + instructionBundles[x] + "\n")
-
-        # keep track of what bytes we're on
-        bundleTokens = instructionBundles[x].split(" ")
-        i = i + len(bundleTokens)
+                # keep track of what bytes we're on
+                bundleTokens = instructionBundles[x].split(" ")
+                i = i + len(bundleTokens)
---
+++ Disassembler/runDisassembler.py
@@ -0,0 +1,4 @@
+from disassembler import disassembler
+
+myDisassembler = disassembler()
+myDisassembler.disassemble("./machineCode.bin", "./Output/test.asm")
